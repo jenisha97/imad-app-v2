7174f2e64dd583e9928e37cb67260904de9e77b6
Jenisha Adlene <adlenejenisha@gmail.com>
2017-02-19 22:25:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article 1', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('Article 1 requested');
 });
-app.get('/article 2', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article 2 requested');
 });
-app.get('/article 3', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article 3 requested');
 });
 
