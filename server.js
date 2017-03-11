f32c90daafd26a42f48b135409cf611341c83a08
Jenisha Adlene <adlenejenisha@gmail.com>
2017-03-11 19:13:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+ var config= {
+     user: 'jenisha97',
+     database:'jenisha97',
+     host:'db.imad.hasura-app.io',
+     port:'5432',
+     password:process.env.DB_PASSWORD
+ }
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +16,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var Pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringyfy(result));
+        }
+    });
+})
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
